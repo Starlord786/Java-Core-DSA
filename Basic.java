a9220ec1b60333a6e9548c9641134d6f63d33781
Shaban <imshabanoffl@gmail.com>
2026-06-10 20:53:07 +0530

    dsa Practice

---
+++ Basic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Basic{
+    public static void main(String[] a){
+        int[] arr = {1, 2, 2, 4, 5};
+
+        HashSet <Integer> set = new HashSet<>();
+
+        for(int nums : arr){
+            set.add(nums);
+        }
+
+        System.out.println(set);
+    }
+}
