853618c890a178d0230f7881667c804ec0e18b32
Shaban <imshabanoffl@gmail.com>
2026-06-23 19:53:33 +0530

    cyclicsort

---
+++ DSA/CyclicSort.java
@@ -0,0 +1,43 @@
+package DSA;
+
+import java.util.Arrays;
+
+public class CyclicSort{
+
+    static void sort(int[] arr){
+
+        int i = 0;
+
+        while(i < arr.length){
+
+            int correct = arr[i] - 1;
+
+            if(arr[i] != arr[correct]){
+                swap(arr, i, correct);  
+            }
+
+            else{
+                i++;
+            }
+
+        }
+
+    }
+
+    public static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+
+    public static void main(String[] a){
+
+        int[] arr = {7,4,1,5,2,6,3};
+
+        sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
